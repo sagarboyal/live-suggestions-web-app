c19af4530819b0e3c506f30087098e98103ffa4c
sagarboyal <sboyal12@gmail.com>
2026-04-18 19:51:45 +0530

    create project using spring boot

---
+++ backend/src/main/java/com/sagarboyal/livesuggest/LiveSuggestApplication.java
@@ -0,0 +1,13 @@
+package com.sagarboyal.livesuggest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveSuggestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveSuggestApplication.class, args);
+	}
+
+}
